6da8b41a5c986ce029301323ee87be85e8ec288c
Joshua Curtiss <curtiss.joshua@student.greenriver.edu>
2024-02-18 01:21:18 -0500

    removed controllers from controllers file, renamed locally but server made copy instead of refactoring

---
+++ model/data-layer.php
@@ -0,0 +1,20 @@
+<?php
+
+
+/*
+ * This file represents the data layer for my Job Application
+ * 328/application/model/data-layer.php
+ */
+
+class DataLayer
+{
+    static function getExperience()
+    {
+        return array('0-2', '2-4', '4+');
+    }
+
+    static function getCondiments()
+    {
+        return array('ketchup', 'mustard', 'mayo', 'sriracha', 'relish');
+    }
+}
